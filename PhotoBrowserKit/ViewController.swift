de0f25b003a853b53d909bf2a11727b884e3bf8d
Zoey Shi <zoey@ZoeydeMacBook-Pro.local>
2017-05-29 13:19:12 +0800

    Initial Commit

---
+++ PhotoBrowserKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoBrowserKit
+//
+//  Created by Zoey Shi on 2017/5/29.
+//  Copyright © 2017年 Zoey Shi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
